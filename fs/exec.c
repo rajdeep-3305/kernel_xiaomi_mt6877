ffc81c1b5043535147bb552c7fb11d6443c95bd8
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2022-10-26 15:48:30 +0200

    BACKPORT: mm: multi-gen LRU: move lru_gen_add_mm() out of IRQ-off region
    lru_gen_add_mm() has been added within an IRQ-off region in the commit
    mentioned below.  The other invocations of lru_gen_add_mm() are not within
    an IRQ-off region.
    
    The invocation within IRQ-off region is problematic on PREEMPT_RT because
    the function is using a spin_lock_t which must not be used within
    IRQ-disabled regions.
    
    The other invocations of lru_gen_add_mm() occur while
    task_struct::alloc_lock is acquired.  Move lru_gen_add_mm() after
    interrupts are enabled and before task_unlock().
    
    Bug: 254441685
    Link: https://lkml.kernel.org/r/20221026134830.711887-1-bigeasy@linutronix.de
    Fixes: bd74fdaea1460 ("mm: multi-gen LRU: support page table walks")
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: "Eric W . Biederman" <ebiederm@xmission.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit dda1c41a07b4a4c3f99b5b28c1e8c485205fe860)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: Ifea2fa277501155dfbc39331c66bda0e6bf4365b
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1047,6 +1047,7 @@ static int exec_mmap(struct mm_struct *mm)
 	if (IS_ENABLED(CONFIG_ARCH_WANT_IRQS_OFF_ACTIVATE_MM))
 		local_irq_enable();
 	tsk->mm->vmacache_seqnum = 0;
+	lru_gen_add_mm(mm);
 	vmacache_flush(tsk);
 	task_unlock(tsk);
 	if (old_mm) {
