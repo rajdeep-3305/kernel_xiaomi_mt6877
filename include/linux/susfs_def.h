f5ca939d6b4e6cae7f852c97f8ed95f0e39b17f8
Renia in RainyLand <rainyxeon@gmail.com>
2025-10-13 17:46:49 +0700

    try to fix mount gap by correcting symbols
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -37,7 +37,7 @@
 #define SUS_SU_WITH_HOOKS 2
 
 #define DEFAULT_KSU_MNT_ID 500000 /* used by mount->mnt_id */
-#define DEFAULT_KSU_MNT_GROUP_ID 000 /* used by mount->mnt_group_id */
+#define DEFAULT_KSU_MNT_GROUP_ID 5000 /* used by mount->mnt_group_id */
 
 /*
  * mount->mnt.susfs_mnt_id_backup => storing original mnt_id of normal mounts or custom sus mnt_id of sus mounts
