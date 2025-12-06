631588edb26d3e5caf7d25191285a7d33b8c357d
RainyXeon <rainyxeon@gmail.com>
2025-12-06 18:06:31 +0700

    FROMGIT: fs: remove unused code from susfs
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2192,13 +2192,6 @@ seqretry:
 				continue;
 			if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
 				continue;
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode
-				&& unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH)
-				&& likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
-				continue;
-			}
-#endif
 		}
 		*seqp = seq;
 		return dentry;
