dc0c63b5f0ab5e838d5e8350c59e9c1a0c0b5017
蒼雨 <rainyxeon@gmail.com>
2026-03-29 13:24:31 +0000

    FROMGIT: treewide: remove susfs for big reimplementation
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -27,10 +27,6 @@
 #include <linux/parser.h>
 #include <linux/fsnotify.h>
 #include <linux/seq_file.h>
-#ifdef CONFIG_KSU_SUSFS
-#include <linux/susfs_def.h>
-#endif
-
 #define DEVPTS_DEFAULT_MODE 0600
 /*
  * ptmx is a new node in /dev/pts and will be unused in legacy (single-
@@ -608,18 +604,8 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
  *
  * Returns whatever was passed as priv in devpts_pty_new for a given inode.
  */
-#ifdef CONFIG_KSU_SUSFS
-extern int ksu_handle_devpts(struct inode*);
-#endif
 void *devpts_get_priv(struct dentry *dentry)
 {
-#ifdef CONFIG_KSU_SUSFS
-	if (likely(susfs_is_current_proc_umounted())) {
-		goto orig_flow;
-	}
-	ksu_handle_devpts(dentry->d_inode);
-orig_flow:
-#endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
--- kernel/sys.c
+++ kernel/sys.c
@@ -699,16 +699,8 @@ error:
 	return retval;
 }
 
-// #ifdef CONFIG_KSU_SUSFS
-// extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
-// #endif
 SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 {
-// #ifdef CONFIG_KSU_SUSFS
-// 	if (ksu_handle_setresuid(ruid, euid, suid)) {
-// 		pr_info("Something wrong with ksu_handle_setresuid()\n");
-// 	}
-// #endif
 	return __sys_setresuid(ruid, euid, suid);
 }
 
@@ -1259,9 +1251,7 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
+extern bool legacy_ebpf __read_mostly;
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1277,9 +1267,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_info("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
-#endif
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
 #endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
