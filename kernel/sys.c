5118ae37b4f2d7671bf8dc5261f890dc69b5b973
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2026-05-16 15:12:04 +0530

    kernel: sys: remove dead #if 0 legacy ebpf uname
    * replaced by CONFIG_KSU_SUSFS_SPOOF_UNAME
    
    Change-Id: If433fc65dc07430355a3b1f49ecdeb4193962e5b
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1261,16 +1261,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-#if 0
-	if (current_uid().val == 0 && 
-		(!strncmp(current->comm, "bpfloader", 9) ||
-		!strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4))) {
-		strcpy(tmp.release, "5.4.186");
-		pr_info("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
-	}
-#endif
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	if (static_branch_likely(&susfs_set_uname_key_true))
 		susfs_spoof_uname(&tmp);
